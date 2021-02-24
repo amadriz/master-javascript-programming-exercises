de3ba603647262322c9a08712534a44236eac2b0
Allan Madriz <ahovenga@gmail.com>
2021-02-24 18:17:37 +0000

    More js exercises

--- exercises/97-keep/app.js
+++ exercises/97-keep/app.js
@@ -1,1 +1,14 @@
 // Write your function here
+function keep(arr, element){
+    newArray = [];
+    for(let i in arr){
+        if(arr[i] === element){
+           newArray.push(arr[i])
+        } 
+    };
+    return newArray;
+
+}
+
+var output = keep([1, 2, 3, 2, 1], 2) 
+console.log(output); //--> [2, 2]
--- exercises/98-getOddLengthWordsAtProperty/app.js
+++ exercises/98-getOddLengthWordsAtProperty/app.js
@@ -1,1 +1,20 @@
 // Write your function here
+
+function getOddLengthWordsAtProperty(obj, key) {
+  var oddArr = [];
+  if (!Array.isArray(obj[key])) {
+    return oddArr;
+  }
+  for (var prop in obj[key]) {
+    if (obj[key][prop].length % 2 === 1) {
+      oddArr.push(obj[key][prop]);
+    }
+  }
+  return oddArr;
+}
+
+var obj = {
+  key: ['It', 'has', 'some', 'words']
+};
+var output = getOddLengthWordsAtProperty(obj, 'key');
+console.log(output); // --> ['has', 'words']
